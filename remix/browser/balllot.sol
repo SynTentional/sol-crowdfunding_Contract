920a618fc8497a782286af64f2443768f95a12d2
Christopher Mullins (Chris) <69275653+SynTentional@users.noreply.github.com>
2023-07-12 15:30:53 -0700

    added contribute fx

--- remix/browser/balllot.sol
+++ remix/browser/balllot.sol
@@ -3,10 +3,17 @@ pragma solidity ^0.8.19;
 contract Campaign {
     address public manager;
     uinit public minimumContribution;
+    address[] public approvers;
 
     // constructor
     function Campaign(uint minimum) public {
         manager = msg.sender;
         minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution); // makes sure user sends more than minimum contribution amt
+        
+        approvers.push(msg.sender); // adds user to approvers array
     }
 }
