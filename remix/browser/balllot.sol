3ffe2693b3f07f35d9d73958dd262dc9d5075db9
Christopher Mullins (Chris) <69275653+SynTentional@users.noreply.github.com>
2023-07-12 15:27:29 -0700

    finished construction fx

--- remix/browser/balllot.sol
+++ remix/browser/balllot.sol
@@ -2,9 +2,11 @@ pragma solidity ^0.8.19;
 
 contract Campaign {
     address public manager;
+    uinit public minimumContribution;
 
     // constructor
-    function Campaign() public {
+    function Campaign(uint minimum) public {
         manager = msg.sender;
+        minimumContribution = minimum;
     }
 }
