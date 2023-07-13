241530e331c469543912f2d93d2d865a3de8b0f5
Christopher Mullins (Chris) <69275653+SynTentional@users.noreply.github.com>
2023-07-12 17:47:48 -0700

    added restricted modifier

--- remix/browser/balllot.sol
+++ remix/browser/balllot.sol
@@ -1,9 +1,21 @@
 pragma solidity ^0.8.19;
 
 contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+
+    Request[] public requests;
     address public manager;
     uinit public minimumContribution;
     address[] public approvers;
+
+    modifier restricted() {
+        require(msg.sender == manager); // makes sure only manager can call this function
+    }
 
     // constructor
     function Campaign(uint minimum) public {
@@ -14,6 +26,10 @@ contract Campaign {
     function contribute() public payable {
         require(msg.value > minimumContribution); // makes sure user sends more than minimum contribution amt
         
-        approvers.push(msg.sender); // adds user to approvers array
+        approvers.push(msg.sender);
+
+
     }
+
+    function createRequest 
 }
